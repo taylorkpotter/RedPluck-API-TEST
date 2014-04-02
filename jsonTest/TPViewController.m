f31aa6bf98c6631a2c37268bf994ce2b0a2394bc
Taylor <taylorkpotter@me.com>
2014-04-01 23:17:04 -0700

    First commit

--- jsonTest/TPViewController.m
+++ jsonTest/TPViewController.m
@@ -8,7 +8,9 @@
 
 #import "TPViewController.h"
 
-@interface TPViewController ()
+@interface TPViewController () <UITableViewDataSource, UITableViewDelegate>
+@property (nonatomic, weak) IBOutlet UITableView *tableView;
+@property (nonatomic) NSMutableArray *filings;
 
 @end
 
@@ -17,7 +19,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    [self downloadFilings];
 }
 
 - (void)didReceiveMemoryWarning
@@ -25,5 +27,39 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+  static NSString *identifier = @"Cell";
+  UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:identifier forIndexPath:indexPath];
+  //Pulling out one filing for the row were about to draw
+  NSDictionary *filing = _filings[indexPath.row];
+  cell.textLabel.text = [filing objectForKey:@"Company Name"];
+  return cell;
+  
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+  // underscore to declare property & count of cells should be equal to filings array.
+  return _filings.count;
+}
+
+//This is a non asyncronous way to download the json code
+- (void) downloadFilings
+{
+  NSString *jsonString = @"http://redpluck.com/v1/tasks/20/items";
+  NSURL *jsonURL = [NSURL URLWithString:jsonString];
+  NSData *jsonData = [NSData dataWithContentsOfURL:jsonURL];
+  //parses json
+  _filings = [NSJSONSerialization JSONObjectWithData:jsonData options:NSJSONReadingMutableContainers error:nil];
+  [_tableView reloadData];
+}
+
+
+
+
+
+
 
 @end
