ec9025762d62447023f61c1ff513e1b5549bdb6e
Taylor <taylorkpotter@me.com>
2014-04-01 22:19:42 -0700

    Initial Commit

---
+++ jsonTest/TPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TPAppDelegate.h
+//  jsonTest
+//
+//  Created by Taylor Potter on 4/1/14.
+//  Copyright (c) 2014 potter.io. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ jsonTest/TPViewController.h
@@ -0,0 +1,13 @@
+//
+//  TPViewController.h
+//  jsonTest
+//
+//  Created by Taylor Potter on 4/1/14.
+//  Copyright (c) 2014 potter.io. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TPViewController : UIViewController
+
+@end
